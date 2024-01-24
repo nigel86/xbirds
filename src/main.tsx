ed990e42ad1281e0c78ae7a7aa7103dc7103b204
NC <nigel86@gmail.com>
2024-01-25 00:06:59 +0800

    fix css

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,55 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import 'bootstrap/dist/css/bootstrap.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import "bootstrap/dist/css/bootstrap.css";
+import "/src/css/styles_birds.css";
+
+import {
+  ThirdwebProvider,
+  metamaskWallet,
+  coinbaseWallet,
+  walletConnect,
+  embeddedWallet,
+  zerionWallet,
+  rainbowWallet,
+} from "@thirdweb-dev/react";
+
+const activeChain = "optimism-goerli";
+
+const container: HTMLElement | null = document.getElementById("root");
+
+if (container) {
+  const root = ReactDOM.createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <ThirdwebProvider
+        activeChain={activeChain}
+        clientId="1a89a74f6ecbe74393203612be0349cc"
+        supportedWallets={[
+          metamaskWallet({ recommended: true }),
+          coinbaseWallet(),
+          walletConnect(),
+          embeddedWallet({
+            auth: {
+              options: ["email", "google", "apple", "facebook"],
+            },
+          }),
+          zerionWallet(),
+          rainbowWallet(),
+        ]}
+        dAppMeta={{
+          name: "ABirdApp",
+          description: "My ABird app description",
+          logoUrl: "https://example.com/logo.png",
+          url: "https://example.com",
+          isDarkMode: true,
+        }}
+      >
+        <App />
+      </ThirdwebProvider>
+    </React.StrictMode>
+  );
+} else {
+  console.error("Container element not found");
+}
