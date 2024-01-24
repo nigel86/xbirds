0c62103b5263fb1dbe4fcff21253cb3bd479cc30
nigel86 <nigel86@users.noreply.github.com>
2024-01-24 16:59:34 +0000

    deploy: 31937dd9f4978d304540a02acea07c4b9141fd00

---
+++ assets/hasERC20Allowance-0775dbeb.browser.esm-rxvH0Cs5.js
@@ -0,0 +1,7 @@
+import{m as l,q as w}from"./index-WHlFM_aG.js";async function g(e,a,t){const n=e.getProvider(),r=(await l(()=>import("./index-WHlFM_aG.js").then(i=>i.dl),__vite__mapDeps([0,1]))).default,s=new w(n,a,r,{},e.storage),o=await e.getSignerAddress(),d=e.address;return(await s.read("allowance",[o,d])).gte(t)}export{g as h};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/index-WHlFM_aG.js","assets/index-GQ9ZpSUZ.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
