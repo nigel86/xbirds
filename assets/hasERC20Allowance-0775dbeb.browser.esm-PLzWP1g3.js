538a99657bf28817e33acfd63b522e470b39b77e
nigel86 <nigel86@users.noreply.github.com>
2024-02-08 16:58:02 +0000

    deploy: 2becd84afd66378d7c28810220ae9e996abea2e2

--- assets/hasERC20Allowance-0775dbeb.browser.esm-PLzWP1g3.js
+++
@@ -1,7 +0,0 @@
-import{m as l,q as w}from"./index-eCU4XEyP.js";async function g(e,a,t){const n=e.getProvider(),r=(await l(()=>import("./index-eCU4XEyP.js").then(i=>i.dl),__vite__mapDeps([0,1]))).default,s=new w(n,a,r,{},e.storage),o=await e.getSignerAddress(),d=e.address;return(await s.read("allowance",[o,d])).gte(t)}export{g as h};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/index-eCU4XEyP.js","assets/index-g3BadOhu.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
