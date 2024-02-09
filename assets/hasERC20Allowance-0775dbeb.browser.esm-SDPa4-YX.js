c68c1c9495c35110bb336fba052f3a6b9f6eda1a
nigel86 <nigel86@users.noreply.github.com>
2024-02-09 08:35:20 +0000

    deploy: c9b628227bf10264c2d2e7b56079cf5780085e48

--- assets/hasERC20Allowance-0775dbeb.browser.esm-SDPa4-YX.js
+++
@@ -1,7 +0,0 @@
-import{m as l,q as w}from"./index-KK14hZXu.js";async function g(e,a,t){const n=e.getProvider(),r=(await l(()=>import("./index-KK14hZXu.js").then(i=>i.dl),__vite__mapDeps([0,1]))).default,s=new w(n,a,r,{},e.storage),o=await e.getSignerAddress(),d=e.address;return(await s.read("allowance",[o,d])).gte(t)}export{g as h};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/index-KK14hZXu.js","assets/index-C_X9rXhj.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
--- assets/setErc20Allowance-5ac91ab4.browser.esm-Yeq85YAW.js
+++
@@ -1,7 +0,0 @@
-import{G as w,m as f,q as N,a8 as n,aZ as l,a_ as h,a$ as c}from"./index-KK14hZXu.js";const r=n.object({}).catchall(n.union([c,n.unknown()])),u=n.union([n.array(r),r]).optional().nullable(),x=n.object({name:n.union([n.string(),n.number()]).optional().nullable(),description:n.string().nullable().optional().nullable(),image:l.nullable().optional(),animation_url:l.optional().nullable()}),p=x.extend({external_url:l.nullable().optional(),background_color:h.optional().nullable(),properties:u,attributes:u}).catchall(n.union([c,n.unknown()])),O=n.union([p,n.string()]),S=p.extend({id:n.string(),uri:n.string(),image:n.string().nullable().optional(),external_url:n.string().nullable().optional(),animation_url:n.string().nullable().optional()});async function F(a,t,e,o){if(w(e))o.value=t;else{const g=(await f(()=>import("./index-KK14hZXu.js").then(_=>_.dl),__vite__mapDeps([0,1]))).default,m=a.getSigner(),b=a.getProvider(),i=new N(m||b,e,g,a.options,a.storage),d=await a.getSignerAddress(),s=a.address;return(await i.read("allowance",[d,s])).lt(t)&&await i.sendTransaction("approve",[s,t]),o}}export{x as B,p as C,O as N,S as a,F as s};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/index-KK14hZXu.js","assets/index-C_X9rXhj.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
