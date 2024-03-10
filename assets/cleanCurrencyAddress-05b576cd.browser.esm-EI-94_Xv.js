5c78d1ec45ec68a979b8afd7157d87f12c47dc17
nigel86 <nigel86@users.noreply.github.com>
2024-03-10 06:19:41 +0000

    deploy: ce6755d537437ae71a2dc48d1b1c4e86f66d3245

--- assets/cleanCurrencyAddress-05b576cd.browser.esm-EI-94_Xv.js
+++
@@ -1,1 +0,0 @@
-import{s as a,av as e}from"./index-R4obJkIf.js";function t(r){return a(r)?e:r}export{t as c};
--- assets/hasERC20Allowance-83f49044.browser.esm-jdEGeniQ.js
+++
@@ -1,7 +0,0 @@
-import{e as l,j as w}from"./index-R4obJkIf.js";async function g(e,a,t){const n=e.getProvider(),r=(await l(()=>import("./index-R4obJkIf.js").then(i=>i.dm),__vite__mapDeps([0,1]))).default,s=new w(n,a,r,{},e.storage),o=await e.getSignerAddress(),d=e.address;return(await s.read("allowance",[o,d])).gte(t)}export{g as h};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/index-R4obJkIf.js","assets/index-fPORRIP4.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
