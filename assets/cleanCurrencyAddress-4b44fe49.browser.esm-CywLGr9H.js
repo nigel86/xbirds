070c63ccaa851f79c9d38f06576dc1db4074b268
nigel86 <nigel86@users.noreply.github.com>
2024-03-17 18:01:31 +0000

    deploy: ad059c5db5e000056a6769779dc2e6fb16cfc485

---
+++ assets/cleanCurrencyAddress-4b44fe49.browser.esm-CywLGr9H.js
@@ -0,0 +1,1 @@
+import{x as a,aw as e}from"./index-DYrDwapy.js";function t(r){return a(r)?e:r}export{t as c};
---
+++ assets/hasERC20Allowance-e2c954ee.browser.esm-DepGpqfy.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/index-DYrDwapy.js","assets/index-O8rLxTsG.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{i as l,m as w}from"./index-DYrDwapy.js";async function g(e,a,t){const n=e.getProvider(),r=(await l(()=>import("./index-DYrDwapy.js").then(d=>d.dy),__vite__mapDeps([0,1]))).default,s=new w(n,a,r,{},e.storage),i=await e.getSignerAddress(),o=e.address;return(await s.read("allowance",[i,o])).gte(t)}export{g as h};
