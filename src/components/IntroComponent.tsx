9c64d8621e4b2b69a932fa957604ff314ad4e840
NC <nigel86@gmail.com>
2024-03-22 14:42:40 +0800

    intro section update

--- src/components/IntroComponent.tsx
+++ src/components/IntroComponent.tsx
@@ -4,8 +4,8 @@ const IntroComponent = () => {
   return (
     <section className="intro-section bg-dark d-flex align-items-center">
       <div className="container px-4">
-        <div className="row  justify-content-center">
-          <div className="col-12 col-md-6 col-lg-7">
+        <div className="row justify-content-center pb-4">
+          <div className="col-12 col-lg-8">
             <div className="badge rounded-pill text-dark mt-4 mb-2 custom-badge">
               <div className="text-uppercase">Live on OP Mainnet</div>
             </div>
@@ -18,7 +18,7 @@ const IntroComponent = () => {
               become part of the XBirds saga!
             </p>
 
-            <div className="d-flex justify-content-center justify-content-md-start mb-3">
+            <div className="d-flex justify-content-center justify-content-md-start mb-3 pb-4">
               <a
                 href="https://galxe.com/XBirds"
                 className="btn d-flex align-items-center justify-content-center px-3 py-2 me-3 custom-outline mb-1 mb-md-0 btn-fixed-size"
@@ -36,17 +36,41 @@ const IntroComponent = () => {
               </a>
             </div>
           </div>
-          <div className="col-12 col-md-1 col-lg-1"></div>
-          <div className="col-8 col-md-5 col-lg-4 d-flex align-items-center my-3">
-            <img
-              src="assets/nft/xbirds-water.jpg"
-              alt="Image"
-              className="img-thumbnail"
-            />
+          <div className="col-12 col-lg-4">
+            <div className="row gx-3 gy-3">
+              <div className="col-6 col-md-3 col-lg-6">
+                <img
+                  src="assets/intro/002.png"
+                  className="img-thumbnail"
+                  alt="..."
+                />
+              </div>
+              <div className="col-6 col-md-3 col-lg-6">
+                <img
+                  src="assets/intro/004.png"
+                  className="img-thumbnail"
+                  alt="..."
+                />
+              </div>
+              <div className="col-6 col-md-3 col-lg-6">
+                <img
+                  src="assets/intro/005.png"
+                  className="img-thumbnail"
+                  alt="..."
+                />
+              </div>
+              <div className="col-6 col-md-3 col-lg-6">
+                <img
+                  src="assets/intro/006.png"
+                  className="img-thumbnail"
+                  alt="..."
+                />
+              </div>
+            </div>
           </div>
         </div>
 
-        <div className="row my-4 justify-content-center">
+        <div className="row pt-4 my-4 justify-content-center">
           <div className="col-12 col-md-4 text-center mb-4">
             <h6 className="mb-0">Whitelist Price</h6>
             <h1 className="display-3 fw-bold text-primary mb-0">Ξ 0.01</h1>
