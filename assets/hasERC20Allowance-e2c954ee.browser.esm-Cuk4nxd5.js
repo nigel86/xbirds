7bc307a9f37bc85ffde03e0bf2b081ac75fe5c93
nigel86 <nigel86@users.noreply.github.com>
2024-04-02 15:32:05 +0000

    deploy: d04efe9406afde3d3c9ebdde50d9ddf2f5ace328

--- assets/hasERC20Allowance-e2c954ee.browser.esm-Cuk4nxd5.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/index-DAW5BKcr.js","assets/index-m9dy2ye2.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{f as l,k as w}from"./index-DAW5BKcr.js";async function g(e,a,t){const n=e.getProvider(),r=(await l(()=>import("./index-DAW5BKcr.js").then(i=>i.dw),__vite__mapDeps([0,1]))).default,s=new w(n,a,r,{},e.storage),o=await e.getSignerAddress(),d=e.address;return(await s.read("allowance",[o,d])).gte(t)}export{g as h};
