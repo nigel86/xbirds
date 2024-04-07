89ca47f9f6c38c0487414cc9fcfa8fe86c68e677
nigel86 <nigel86@users.noreply.github.com>
2024-04-07 16:09:41 +0000

    deploy: 4645f6414120a9b24a9fa3284993760f2dc4f2c8

--- assets/hasERC20Allowance-e2c954ee.browser.esm-B_FZIe33.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/index-CQ-XxOMg.js","assets/index-Dx_cvKYb.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{i as l,m as w}from"./index-CQ-XxOMg.js";async function g(e,a,t){const n=e.getProvider(),r=(await l(()=>import("./index-CQ-XxOMg.js").then(d=>d.dy),__vite__mapDeps([0,1]))).default,s=new w(n,a,r,{},e.storage),i=await e.getSignerAddress(),o=e.address;return(await s.read("allowance",[i,o])).gte(t)}export{g as h};
