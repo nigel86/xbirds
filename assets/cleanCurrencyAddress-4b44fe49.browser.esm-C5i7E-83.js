688f35e18cbf3d86a5dce184e40e00b8253f2c99
nigel86 <nigel86@users.noreply.github.com>
2024-04-08 11:09:11 +0000

    deploy: e446acfe4f68727ae0f28cf526b8f801a6f8a35a

--- assets/cleanCurrencyAddress-4b44fe49.browser.esm-C5i7E-83.js
+++
@@ -1,1 +0,0 @@
-import{x as a,aw as e}from"./index-DtF3FnBX.js";function t(r){return a(r)?e:r}export{t as c};
--- assets/hasERC20Allowance-e2c954ee.browser.esm-DUSCKBw3.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/index-DtF3FnBX.js","assets/index-BJqcV4xN.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{i as l,m as w}from"./index-DtF3FnBX.js";async function g(e,a,t){const n=e.getProvider(),r=(await l(()=>import("./index-DtF3FnBX.js").then(d=>d.dy),__vite__mapDeps([0,1]))).default,s=new w(n,a,r,{},e.storage),i=await e.getSignerAddress(),o=e.address;return(await s.read("allowance",[i,o])).gte(t)}export{g as h};
